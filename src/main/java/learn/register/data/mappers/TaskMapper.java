ad901d35b724b995c0b610d7b09378e664d9237c
Miguel Galindo <miguelgalindo1214@gmail.com>
2024-10-07 12:48:20 -0400

    Adding task mapper to map rows to task object

--- src/main/java/learn/register/data/mappers/TaskMapper.java
+++ src/main/java/learn/register/data/mappers/TaskMapper.java
@@ -1,2 +1,20 @@
-package learn.register.data.mappers;public class TaskMapper {
+package learn.register.data.mappers;
+
+import learn.register.models.Task;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TaskMapper implements RowMapper<Task> {
+
+    @Override
+    public Task mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Task task = new Task();
+        task.setId(resultSet.getLong("id"));
+        task.setTitle(resultSet.getString("title"));
+        task.setDescription(resultSet.getString("description"));
+        task.setStatus(resultSet.getString("status"));
+        task.setUserId(resultSet.getLong("user_id"));
+        return task;
+    }
 }
