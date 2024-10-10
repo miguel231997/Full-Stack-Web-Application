12f6c0c9b3dbdc116fe1bdcc60fd052dba7d7746
Miguel Galindo <miguelgalindo1214@gmail.com>
2024-10-10 14:39:00 -0400

    Added Admin Controller to handle admin roles

---
+++ src/main/java/learn/register/controllers/AdminController.java
@@ -0,0 +1,42 @@
+package learn.register.controllers;
+
+import learn.register.models.AppUser;
+import learn.register.security.AppUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+
+public class AdminController {
+
+    private final AppUserService appUserService;
+
+    public AdminController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    // GET endpoint to retrieve all users, restricted to admins only
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/users")
+    public ResponseEntity<List<AppUser>> getAllUsers() {
+        List<AppUser> users = appUserService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    //DELETE endpoint
+    @PreAuthorize(("hasRole('ROLE_ADMIN')"))
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        boolean success = appUserService.deleteUser(id);
+        if(success) {
+            return ResponseEntity.ok("User deleted SuccessDully");
+        } else {
+            return ResponseEntity.status(404).body("User not found");
+        }
+    }
+}
